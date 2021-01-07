8385c7c42917d4e15ead25a0022b6c214cdb22f1
Eric Clack <eric@bn7.net>
2021-01-07 21:18:46 +0000

    Github banner

--- doc/conf.py
+++ doc/conf.py
@@ -86,7 +86,11 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'github_banner': True,
+    'github_user': 'ericclack',
+    'github_repo': 'pygamezero_animals',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
