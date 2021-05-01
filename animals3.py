1096d85d86500c6474caea96ee7b77006a86d278
Eric Clack <eric@bn7.net>
2021-05-01 16:59:39 +0100

    Version 3 = sheep dog

--- animals3.py
+++ animals3.py
@@ -1,130 +1,73 @@
 import random, math, time
-from enum import Enum
-from animals_lib import *
-
+import pygame
 WIDTH = 800
 HEIGHT = 600
-MAX_SPEED = 1.5
-
-class Status(Enum):
-    DEAD = 0
-    ALIVE = 1
 
 class Animal(Actor):
 
     all = []
 
-    def __init__(self, what):
-        super(Animal, self).__init__('%s.png' % what)
-        self.what = what
-        self.x = random.randint(0, WIDTH)
-        self.y = random.randint(0, HEIGHT)
-        self.direction = random.uniform(0, math.pi * 2)
-        self.max_speed = MAX_SPEED
-        self.speed = random.uniform(2,self.max_speed)
-        self.status = Status.ALIVE
-
+    def __init__(self, img):
+        super(Animal, self).__init__(img)
+        self.x = random.randint(WIDTH*1/5, WIDTH*4/5)
+        self.y = random.randint(HEIGHT*1/5, HEIGHT*4/5)
         Animal.all.append(self)
 
     def move(self):
-        if self.status == Status.DEAD:
-            return
-
-        # Our default direction vector
-        dx, dy = xy_from_angle_mag(self.direction, self.speed)
-
-        # Change our direction and speed according to other animals
         for o in self.other_animals():
-            fx, fy = xy_from_angle_mag(self.angle_to(o), self.attraction_to(o))
-            dx += fx
-            dy += fy
-
-        # Uptdate direction with attractions above
-        self.direction, self.speed = angle_mag_from_xy(dx, dy)
-        # Don't move too fast
-        self.speed = min(self.max_speed, self.speed)
-        # Create the actual movement vector given that we might have reduced speed
-        dx, dy = xy_from_angle_mag(self.direction, self.speed)
-
-        # Move
-        self.x += dx
-        self.y += dy
-
-        self.stay_on_screen()
-
-    def stay_on_screen(self):
-        if self.x < 0:         self.x = 0
-        elif self.x > WIDTH:   self.x = WIDTH
-        if self.y < 0:         self.y = 0
-        elif self.y > HEIGHT:  self.y = HEIGHT
+            self.move_by_attraction(o)
 
     def other_animals(self):
         """All the animals except us"""
         return [a for a in Animal.all if a != self]
 
+    def move_by_attraction(self, other):
+        angle = self.angle_to(other)
+        fx = math.cos(angle) * self.attraction_to(other)
+        fy = math.sin(angle) * self.attraction_to(other)
+        self.x += fx
+        self.y += fy
+
     def distance_to(self, other):
+        # Distances
+        dx = self.x - other.x
+        dy = self.y - other.y
         # Pythagoras
-        return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
+        return math.sqrt(dx**2 + dy**2)
 
     def angle_to(self, other):
         # 0 is left, pi/2 is up, pi is right, -pi/2 down
         return math.atan2(other.y - self.y, other.x - self.x)
 
     def attraction_to(self, other):
-        # No attraction by default
-        return 0
+        # Attraction until we get too close
+        d = self.distance_to(other)
+        return 0.2 * -math.cos(d/40)
+
 
 class Sheep(Animal):
+
     def __init__(self):
-        super().__init__('sheep')
+        super().__init__('sheep.png')
 
-    def attraction_to(self, other):
-        """Positive number means attraction, negative repulsion"""
-        d = self.distance_to(other)
 
-        if other.what == 'sheep':
-            # Attraction until we get too close
-            d = self.distance_to(other)
-            return (-30/d) + 0.01*d
+class SheepDog(Animal):
 
-        elif other.what == 'wolf':
-            # A wolf, run away!
-            return -400 / d
-
-class Wolf(Animal):
     def __init__(self):
-        super().__init__('wolf')
-        self.max_speed = MAX_SPEED
+        super().__init__('dog.png')
 
     def move(self):
-        super().move()
-        others = self.other_animals()
+        self.x, self.y = pygame.mouse.get_pos()
 
-        # Caught a sheep?
-        i = self.collidelist(others)
-        if i != -1 and others[i].what == 'sheep':
-            others[i].status = Status.DEAD
-
-    def attraction_to(self, other):
-        # Attraction gets stronger the closer the other gets
-        d = self.distance_to(other)
-        if other.what == 'sheep':
-            if other.status == Status.DEAD:
-                return 0
-            else:
-                return 15 / (d / 10) ** 2
-        return 0
-
-# Make animals
-for i in range(5):
+# Make some animals
+for i in range(15):
     Sheep()
-Wolf()
-Wolf()
+
+SheepDog()
 
 def draw():
     screen.blit('southdowns.jpeg', (0,0))
     for a in Animal.all: a.draw()
-    #time.sleep(2)
 
 def update():
     for a in Animal.all: a.move()
